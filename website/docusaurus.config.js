5e75a5e4922f1194fdcb7d9bcf503823bf69f704
Jules Fouchy <jules.fouchy@ntymail.com>
2021-12-23 19:34:55 +0100

    [docusaurus.config.js] Keep adding user-configurable values

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,112 +1,96 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github')
-const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-const user_config = require('../../website.config')
+const user_config = require("../../website.config");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   url: `https://${user_config.github_account}.github.io/`,
   baseUrl: `/${user_config.repo}/`,
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
-  favicon: 'img/favicon.ico',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "throw",
+  favicon: "img/favicon.ico",
   organizationName: user_config.github_account,
   projectName: user_config.repo,
   themeConfig: {
     navbar: {
-      title: '',
+      title: "",
       logo: {
-        alt: 'Site Logo',
-        src: 'img/favicon-32x32.png',
+        alt: "Site Logo",
+        src: "img/favicon-32x32.png",
       },
       items: [
         ...user_config.navbar_items,
         {
-          href: `https://github.com/${user_config.github_account}/${
-              user_config.repo}/issues`,
-          position: 'right',
-          className: 'header-issues-link',
-          'aria-label': 'GitHub issues',
+          href: `https://github.com/${user_config.github_account}/${user_config.repo}/issues`,
+          position: "right",
+          className: "header-issues-link",
+          "aria-label": "GitHub issues",
         },
       ],
     },
-    footer: {
-      style: 'dark',
+    footer: user_config.footer || {
+      style: "dark",
       links: [
         {
-          title: 'Other classes',
+          title: "Other classes",
           items: [
             {
-              label: 'Math for Art and Computer Graphics',
-              to: 'https://julesfouchy.github.io/Learn--Math-for-Art-and-Computer-Graphics',
+              label: "Math for Art and Computer Graphics",
+              to: "https://julesfouchy.github.io/Learn--Math-for-Art-and-Computer-Graphics",
             },
             {
-              label: 'C++ and Dev Practices',
-              to: 'https://julesfouchy.github.io/Learn--Cpp-And-Dev-Practices/',
+              label: "C++ and Dev Practices",
+              to: "https://julesfouchy.github.io/Learn--Cpp-And-Dev-Practices/",
             },
           ],
         },
         {
-          title: 'Raise an issue!',
+          title: "Raise an issue!",
           items: [
             {
-              label: 'Ask a question',
-              to: 'https://github.com/JulesFouchy/Learn--Math-for-Art-and-Computer-Graphics/issues',
+              label: "Ask a question",
+              to: `https://github.com/${user_config.github_account}/${user_config.repo}/issues`,
             },
             {
-              label: 'Report a mistake',
-              to: 'https://github.com/JulesFouchy/Learn--Math-for-Art-and-Computer-Graphics/issues',
+              label: "Report a mistake",
+              to: `https://github.com/${user_config.github_account}/${user_config.repo}/issues`,
             },
             {
-              label: 'Give me feedback',
-              to: 'https://github.com/JulesFouchy/Learn--Math-for-Art-and-Computer-Graphics/issues',
+              label: "Give me feedback",
+              to: `https://github.com/${user_config.github_account}/${user_config.repo}/issues`,
             },
           ],
         },
         {
-          title: 'Contact Me',
-          items: [
-            {
-              label: 'Discord',
-              to: 'https://discord.com/users/372812330742054914',
-            },
-            {
-              label: 'E-Mail',
-              to: 'mailto:jules.fouchy@ntymail.com',
-            },
-            {
-              label: 'GitHub',
-              to: 'https://github.com/JulesFouchy',
-            },
-          ],
+          title: "Contact Me",
+          items: user_config.contact_info,
         },
       ],
-      copyright: `Copyright © ${
-          new Date()
-              .getFullYear()}. Built with <a href="https://docusaurus.io/">Docusaurus</a>.
-              <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-              `,
+      copyright:
+        `Copyright © ${new Date().getFullYear()}. Built with <a href="https://docusaurus.io/">Docusaurus</a>.` +
+          user_config.extra_copyright || "",
     },
     prism: {
       theme: lightCodeTheme,
       darkTheme: darkCodeTheme,
-      //   additionalLanguages: ['typescript'],
+      additionalLanguages: user_config.syntax_highlight_for || [],
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          path: '../../docs',
-          routeBasePath: 'docs',
-          sidebarPath: require.resolve('./sidebars.js'),
+          path: user_config.path_to_docs || "../../docs",
+          routeBasePath: "docs",
+          sidebarPath: require.resolve("./sidebars.js"),
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
   ],
-  ...user_config.docusaurus_settings
+  ...user_config.docusaurus_config,
 };
