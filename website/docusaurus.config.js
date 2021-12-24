c0b5932d31e981ea8fcb5ee31f7391c708d5565c
Jules Fouchy <jules.fouchy@ntymail.com>
2021-12-24 01:28:17 +0100

    Use the newest Docusaurus setup, with Typescript

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,14 +1,12 @@
+// @ts-check
+
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-// const get_user_config = require("../../website.config");
 const user_config = require("../../website.config")();
 
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
-// module.exports = async function configCreatorAsync() {
-module.exports = {
-  //   const user_config = await get_user_config();
-  //   return {
+/** @type {import('@docusaurus/types').Config} */
+const config = {
   title: user_config.title,
   url: `https://${user_config.github_account.toLowerCase()}.github.io/`,
   baseUrl: `/${user_config.repo}/`,
@@ -17,71 +15,80 @@ module.exports = {
   favicon: "img/favicon.ico",
   organizationName: user_config.github_account,
   projectName: user_config.repo,
-  themeConfig: {
-    navbar: {
-      title: "",
-      logo: {
-        alt: "Site Logo",
-        src: "img/favicon-32x32.png",
-      },
-      items: [
-        ...user_config.navbar_items,
-        {
-          href: `https://github.com/${user_config.github_account}/${user_config.repo}/issues`,
-          position: "right",
-          className: "header-issues-link",
-          "aria-label": "GitHub issues",
-        },
-      ],
-    },
-    footer: user_config.footer || {
-      style: "dark",
-      links: [
-        {
-          title: "Other classes",
-          items: user_config.classes
-            .filter((c) => c.name !== user_config.title)
-            .map((c) => ({
-              label: c.name,
-              to: c.url,
-            })),
-        },
-        {
-          title: "Raise an issue!",
-          items: [
-            {
-              label: "Ask a question",
-              to: `https://github.com/${user_config.github_account}/${user_config.repo}/issues`,
-            },
-            {
-              label: "Report a mistake",
-              to: `https://github.com/${user_config.github_account}/${user_config.repo}/issues`,
-            },
-            {
-              label: "Give me feedback",
-              to: `https://github.com/${user_config.github_account}/${user_config.repo}/issues`,
-            },
-          ],
-        },
-        {
-          title: "Contact Me",
-          items: user_config.contact_info,
-        },
-      ],
-      copyright:
-        `Copyright © ${new Date().getFullYear()}. Built with <a href="https://docusaurus.io/">Docusaurus</a>.` +
-          user_config.extra_copyright || "",
-    },
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-      additionalLanguages: user_config.syntax_highlight_for || [],
-    },
+  staticDirectories: ["static", "../../static"],
+  customFields: {
+    main_page_link: user_config.main_page_link,
   },
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "",
+        logo: {
+          alt: "Site Logo",
+          src: "img/favicon-32x32.png",
+        },
+        items: [
+          ...user_config.navbar_items,
+          {
+            href: `https://github.com/${user_config.github_account}/${user_config.repo}/issues`,
+            position: "right",
+            className: "header-issues-link",
+            "aria-label": "GitHub issues",
+          },
+        ],
+      },
+      footer: user_config.footer || {
+        style: "dark",
+        links: [
+          {
+            title: "Other classes",
+            items: user_config.classes
+              .filter((c) => c.name !== user_config.title)
+              .map((c) => ({
+                label: c.name,
+                to: c.url,
+              })),
+          },
+          {
+            title: "Raise an issue!",
+            items: [
+              {
+                label: "Ask a question",
+                to: `https://github.com/${user_config.github_account}/${user_config.repo}/issues`,
+              },
+              {
+                label: "Report a mistake",
+                to: `https://github.com/${user_config.github_account}/${user_config.repo}/issues`,
+              },
+              {
+                label: "Give me feedback",
+                to: `https://github.com/${user_config.github_account}/${user_config.repo}/issues`,
+              },
+            ],
+          },
+          {
+            title: "Contact Me",
+            items: user_config.contact_info,
+          },
+        ],
+        copyright:
+          `Copyright © ${new Date().getFullYear()}. Built with <a href="https://docusaurus.io/">Docusaurus</a>.` +
+            user_config.extra_copyright || "",
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: user_config.syntax_highlight_for || [],
+      },
+    }),
+
   presets: [
     [
-      "@docusaurus/preset-classic",
-      {
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
           path: user_config.path_to_docs || "../../docs",
           routeBasePath: "docs",
@@ -90,9 +97,11 @@ module.exports = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
-      },
+      }),
     ],
   ],
+
   ...user_config.docusaurus_config,
 };
-// };
+
+module.exports = config;
