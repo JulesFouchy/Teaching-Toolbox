5e0fddc53df1b87d7fab5c8648b03805b2b6f45e
Jules Fouchy <jules.fouchy@ntymail.com>
2021-12-24 04:12:12 +0100

    [website] Add a searchbar

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -50,6 +50,8 @@ const config = {
     },
   ],
 
+  plugins: [require.resolve("docusaurus-lunr-search")],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
