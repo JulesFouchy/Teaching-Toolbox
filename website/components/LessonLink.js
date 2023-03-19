0187b73dce58b934f471218a47faec2fc2e64d37
Jules Fouchy <jules.fouchy@ntymail.com>
2023-03-19 17:46:58 +0100

    Fix typo

--- website/components/LessonLink.js
+++ website/components/LessonLink.js
@@ -4,7 +4,7 @@ import lessons from "@site/.docusaurus/plugin--lessons-list/default/lessons.json
 export default function LessonLink({ slug, text, anchor }) {
   const lesson = lessons.find((lesson) => lesson.slug === slug)
   if (lesson === undefined) {
-    console.error(`Broken link to inexistant lesson "${slug}"`)
+    console.error(`Broken link to inexistent lesson "${slug}"`)
     throw ""
   }
   return <a href={lesson.link + (anchor || "")}>{text || lesson.title}</a>
